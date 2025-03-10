558111066a5a524d738cc7090042a0a470cccf8f
Tiziano Mendez <166566359+Tmendezz@users.noreply.github.com>
2025-03-10 17:39:00 -0300

    Agregamos exlusion .APP

---
+++ GestorBitwise.APP/Controllers/CategoriaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestorBitwise.APP.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
